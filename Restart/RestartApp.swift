0532db2a2720244509e9384738aad23322269981
Sean Murphy <sdmurphy.1999@gmail.com>
2022-06-22 19:53:37 -0400

    add app assets

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Sean Murphy on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
